772c2cd89112b92f27d05b89511a7e2f45db9e3f
1330Petrel <wuqw23@mails.tsinghua.edu.cn>
2025-03-28 23:33:20 +0800

    appoint version for dependencies

--- sdk/cpp/xmake.lua
+++ sdk/cpp/xmake.lua
@@ -1,10 +1,10 @@
 add_rules("mode.debug", "mode.release")
 
-add_requires("cxxopts")
-add_requires("glaze")
-add_requires("libhv", {configs = {http_server = false}})
-add_requires("magic_enum")
-add_requires("spdlog")
+add_requires("cxxopts >=3.2.1")
+add_requires("glaze >=4.4.3")
+add_requires("libhv >=1.3.3", {configs = {http_server = false}})
+add_requires("magic_enum >=0.9.7")
+add_requires("spdlog >=1.15.1")
 
 target("agent")
     set_kind("binary")
